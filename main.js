ef7795ece5639cec84ac2a73b84a9c6983330627
Facundo Barneche <facu_barneche@hotmail.com.ar>
2020-10-20 20:45:38 -0300

    En esta modificacion se le agrego al programa unos blucles para que no se repita la forma verbal y el verbo ya pregunta...

--- main.js
+++ main.js
@@ -14,44 +14,62 @@ Lograr que no se repitan las preguntas...
 const sForm = ['add', 'borrowed', 'clap', 'cook', 'enjoy', 'hear', 'help', 'kiss', 'lower', 'plan'];
 const sPast = ['added', 'borrowed', 'clapped', 'cooked', 'enjoyed', 'heard', 'helped', 'kissed', 'lowered', 'planned'];
 const pParticiple = ['added', 'borrowed', 'clapped', 'cooked', 'enjoyed', 'heard', 'helped', 'kissed', 'lowered', 'planned'];
-const spanishVerb = ['agregar','tomar prestado','aplaudir','cocinar','disfrutar','oír','ayudar','besar','reducir','planear'];
+const spanishVerb = ['agregar', 'tomar prestado', 'aplaudir', 'cocinar', 'disfrutar', 'oír', 'ayudar', 'besar', 'reducir', 'planear'];
 let aux = "";
 let cont = 0;
 let nVerb;
 let nForms;
 
 const forms = () => {
-    nForms = Math.floor(Math.random() * (0,3));
+    nForms = Math.floor(Math.random() * (0, 3));
     return nForms;
 }
 const verbs = () => {
-    nVerb = Math.floor(Math.random() * (0,10));
+    nVerb = Math.floor(Math.random() * (0, 10));
     return nVerb;
 }
 
+let used = [];
+let repeat = [""];
+
 for (i = 0; i < 10; i++) {
-    switch (forms()) {
-        case 0:
-          aux = prompt(`how do you say "${spanishVerb[verbs()]}" in its simple form?`).toLowerCase();
-         if(aux === sForm[nVerb]){
-             cont++;
-         }
-            break;
-        case 1:
-            aux = prompt(`how do you say "${spanishVerb[verbs()]}" in its simple past?`).toLowerCase();
-            if(aux === sPast[nVerb]){
-                cont++;
+    forms();
+    verbs();
+    used[i] = nForms.toString() + nVerb.toString();
+    start:
+        for (j = 0; j < repeat.length; j++) {
+            if (used[i] == repeat[j]) {  
+                i--;
+                break;
             }
-            break;
-        case 2:
-            aux = prompt(`how do you say "${spanishVerb[verbs()]}" in its past participle?`).toLowerCase();
-            if(aux === pParticiple[nVerb]){
-                cont++;
+            if (i == used.length-1 && j == repeat.length-1) {
+                repeat.push(used[i]);
+                
+                switch (nForms) {
+                    case 0:
+                        aux = prompt(`how do you say "${spanishVerb[nVerb]}" in its simple form?`).toLowerCase();
+                        if (aux === sForm[nVerb]) {
+                            cont++;
+                        }
+                        break;
+                    case 1:
+                        aux = prompt(`how do you say "${spanishVerb[nVerb]}" in its simple past?`).toLowerCase();
+                        if (aux === sPast[nVerb]) {
+                            cont++;
+                        }
+                        break;
+                    case 2:
+                        aux = prompt(`how do you say "${spanishVerb[nVerb]}" in its past participle?`).toLowerCase();
+                        if (aux === pParticiple[nVerb]) {
+                            cont++;
+                        }
+                        break;
+                    default:
+            
+                }
+                break start;
             }
-            break;
-            default:
-              
-    }
+        }
 }
 
-document.write(`Ha acertado un total de ${cont}/10 verbos <br>Tu nota final es ${((cont*10)/10).toFixed(2)}`);
+document.write(`Ha acertado un total de ${cont}/10 verbos <br>Tu nota final es ${((cont * 10) / 10).toFixed(2)}`);
