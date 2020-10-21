0c87965a3adc5e8ae423e2aed4cfd3128545e81b
Facundo Barneche <facu_barneche@hotmail.com.ar>
2020-10-21 20:50:15 -0300

    Esta version ya puede predecir los errores y te devuelve los datos correctamente

--- main.js
+++ main.js
@@ -8,68 +8,102 @@ Vamos a crear un proyecto que en un vector guarde algunos verbos y que el usuari
 agregar nota final, con los aciertos
 ------------------------------------------
 Lograr que no se repitan las preguntas...
+-----------------------------------------
+mostrar respuestas INCORRECTAS!
 
 */
 
-const sForm = ['add', 'borrowed', 'clap', 'cook', 'enjoy', 'hear', 'help', 'kiss', 'lower', 'plan'];
+const sForm = ['add', 'borrow', 'clap', 'cook', 'enjoy', 'hear', 'help', 'kiss', 'lower', 'plan'];
 const sPast = ['added', 'borrowed', 'clapped', 'cooked', 'enjoyed', 'heard', 'helped', 'kissed', 'lowered', 'planned'];
 const pParticiple = ['added', 'borrowed', 'clapped', 'cooked', 'enjoyed', 'heard', 'helped', 'kissed', 'lowered', 'planned'];
 const spanishVerb = ['agregar', 'tomar prestado', 'aplaudir', 'cocinar', 'disfrutar', 'oír', 'ayudar', 'besar', 'reducir', 'planear'];
+let wrongAnswer = [], okAnswer = [];
 let aux = "";
 let cont = 0;
 let nVerb;
 let nForms;
 
-const forms = () => {
+
+const forms = () => { //numero aleatorio para la forma verbal
     nForms = Math.floor(Math.random() * (0, 3));
     return nForms;
 }
-const verbs = () => {
+const verbs = () => {//numero aleatorio para el verbo
     nVerb = Math.floor(Math.random() * (0, 10));
     return nVerb;
 }
 
-let used = [];
-let repeat = [""];
+const supr = () => {//suprime undefined e imprime respuestas incorrectas y su respectivas respuestas correctas
+    for (i = 0; i < wrongAnswer.length; i++) {
+        if (wrongAnswer[i] === undefined) {
+            okAnswer.splice(i, 1);
+            wrongAnswer.splice(i, 1);
+            --i
+        } else {
+            document.write(`Respuesta Incorrecta N° ${i + 1}: "${wrongAnswer[i]}"
+             -----> Respuesta Correcta: "${okAnswer[i]}"<br>`);
+        }
+    }
+    document.write(`<br>Ha acertado un total de ${cont}/20 verbos <br>Tu nota final es
+     ${((cont * 10) / 20).toFixed(2)}<br>`);
+}
 
-for (i = 0; i < 10; i++) {
+
+for (i = 0; i < 20; i++) {
+    let used = [];
+    let repeat = [""];
     forms();
     verbs();
     used[i] = nForms.toString() + nVerb.toString();
+
     start:
-        for (j = 0; j < repeat.length; j++) {
-            if (used[i] == repeat[j]) {  
-                i--;
-                break;
-            }
-            if (i == used.length-1 && j == repeat.length-1) {
-                repeat.push(used[i]);
-                
-                switch (nForms) {
-                    case 0:
-                        aux = prompt(`how do you say "${spanishVerb[nVerb]}" in its simple form?`).toLowerCase();
-                        if (aux === sForm[nVerb]) {
-                            cont++;
-                        }
-                        break;
-                    case 1:
-                        aux = prompt(`how do you say "${spanishVerb[nVerb]}" in its simple past?`).toLowerCase();
-                        if (aux === sPast[nVerb]) {
-                            cont++;
-                        }
-                        break;
-                    case 2:
-                        aux = prompt(`how do you say "${spanishVerb[nVerb]}" in its past participle?`).toLowerCase();
-                        if (aux === pParticiple[nVerb]) {
-                            cont++;
-                        }
-                        break;
-                    default:
-            
-                }
-                break start;
-            }
+    for (j = 0; j < repeat.length; j++) {
+        if (used[i] == repeat[j]) {
+            i--;
+            break;
         }
+        if (i == used.length - 1 && j == repeat.length - 1) {
+            repeat.push(used[i]);
+
+            switch (nForms) {
+                case 0:
+                    aux = prompt(`how do you say "${spanishVerb[nVerb]}" in its simple form?`).toLowerCase();
+                    if (aux === sForm[nVerb]) {
+                        cont++;
+                    } else {
+                        wrongAnswer[i] = aux;
+                        okAnswer[i] = sForm[nVerb];
+
+                    }
+                    break;
+                case 1:
+                    aux = prompt(`how do you say "${spanishVerb[nVerb]}" in its simple past?`).toLowerCase();
+                    if (aux === sPast[nVerb]) {
+                        cont++;
+                    } else {
+                        wrongAnswer[i] = aux;
+                        okAnswer[i] = sPast[nVerb];
+
+                    }
+                    break;
+                case 2:
+                    aux = prompt(`how do you say "${spanishVerb[nVerb]}" in its past participle?`).toLowerCase();
+                    if (aux === pParticiple[nVerb]) {
+                        cont++;
+                    } else {
+                        wrongAnswer[i] = aux;
+                        okAnswer[i] = pParticiple[nVerb];
+
+                    }
+                    break;
+                default:
+
+            }
+            break start;
+        }
+    }
 }
 
-document.write(`Ha acertado un total de ${cont}/10 verbos <br>Tu nota final es ${((cont * 10) / 10).toFixed(2)}`);
+supr();
+
+
